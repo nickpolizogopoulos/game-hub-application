fe0c685071461f742d6b8cafa78b2361af93c156
nickpolizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2023-06-10 07:15:21 +0300

    Build sort selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
 
 export interface GameQuery {
@@ -28,7 +29,7 @@ const App = () => {
         lg: '200px 1fr'
       }}
     >
-      <GridItem area="nav">
+      <GridItem area="nav" marginBottom={5}>
         <NavBar />
       </GridItem>
       <Show above="lg">
@@ -42,9 +43,12 @@ const App = () => {
         </GridItem> 
       </Show>
       <GridItem area="main">
-        <PlatformSelector
-          selectedPlatform={gameQuery.platform}
-          onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})} />
+        <HStack spacing={5} paddingLeft={3.5} marginBottom={2}>
+          <PlatformSelector
+            selectedPlatform={gameQuery.platform}
+            onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})} />
+            <SortSelector />
+        </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,32 @@
+import {
+    Button,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList
+} from "@chakra-ui/react"
+import { BsChevronDown } from "react-icons/bs"
+
+
+const SortSelector = () => {
+
+    
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown />}
+              >
+              Order by Relevance
+              </MenuButton>
+            <MenuList>
+                <MenuItem>Relevance</MenuItem>
+                <MenuItem>Date Added</MenuItem>
+                <MenuItem>Name</MenuItem>
+                <MenuItem>Release Date</MenuItem>
+                <MenuItem>Populatiry</MenuItem>
+                <MenuItem>Average Rating</MenuItem>
+            </MenuList>
+        </Menu>
+      )
+}
+
+export default SortSelector
